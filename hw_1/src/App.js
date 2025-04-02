475906f32437d4cb30b207e678a8002993a382ec
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2025-04-02 05:02:59 +0400

    very very simple task

--- hw_1/src/App.js
+++ hw_1/src/App.js
@@ -1,8 +1,17 @@
+import React from "react";
+import Message from "./Message";
+import './Style.css';
 
 function App() {
+  const messages = [
+    {content: 'hello world'},
+    {content: 'hello 2 world'},
+  ];
   return (
     <div className="App">
-      hello
+      {messages.map((message, index) => (
+        <Message key={index} content={message.content} />
+      ))}
     </div>
   );
 }
---
+++ hw_1/src/Message.jsx
@@ -0,0 +1,10 @@
+
+function Message({content}) {
+    return ( 
+        <div className="box">
+            {content}
+        </div>
+     );
+}
+
+export default Message;
