99ed52028b9c0c2de5423653073bd9b20306a7b9
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2025-05-04 21:35:23 +0400

    homework 7 createAsyncThunk and added Math.random() to call fetchTasks.rejected

--- homeworks/src/hw_7_Thunk/features/tasksSlice.js
+++ homeworks/src/hw_7_Thunk/features/tasksSlice.js
@@ -1,12 +1,33 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { initialTasks } from '../tasks';
 
-// Имитация асинхронной загрузки задач
-export const fetchTasks = createAsyncThunk('tasks/fetchTasks', async () => {
-    // Имитируем задержку сети
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    return initialTasks;
-});
+// Имитация асинхронной загрузки задач - вызываем fetchTasks.fulfilled
+// export const fetchTasks = createAsyncThunk('tasks/fetchTasks', async () => {
+//     // Имитируем задержку сети
+//     await new Promise(resolve => setTimeout(resolve, 1000));
+//     return initialTasks;
+// });
+
+// имитация не удачной загрузки - вызываем fetchTasks.rejected
+// export const fetchTasks = createAsyncThunk('tasks/fetchTasks', async () => {
+//     await new Promise((resolve, reject) => {
+//       setTimeout(() => reject(new Error('имитация не удачной загрузки - вызываем fetchTasks.rejected!')), 1000); 
+//     });
+//   });
+
+
+  export const fetchTasks = createAsyncThunk('tasks/fetchTasks', async () => {
+    // Имитируем случайный успех/ошибку
+    const isSuccess = Math.random() > 0.7;
+  
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  
+    if (isSuccess) {
+      return initialTasks; // Успех → action.payload = initialTasks
+    } else {
+      throw new Error('Failed to load! Try again!'); // Ошибка → action.error
+    }
+  });
 
 const tasksSlice = createSlice({
     name: 'tasks',
